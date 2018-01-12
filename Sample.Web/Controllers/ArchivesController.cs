5fa5f6091148f1b8956eb5893c6b1bb1b8cee6f1
bruse <ograss@qq.com>
2018-01-12 20:29:06 +0800

    添加文章详情页

--- Sample.Web/Controllers/ArchivesController.cs
+++ Sample.Web/Controllers/ArchivesController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Sample.IBLL;
+using Sample.WebHelper;
+using Sample.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,22 @@ namespace Sample.Web.Controllers
 {
     public class ArchivesController : Controller
     {
+        IArticleService _articleService = OperateHelper.Current._serviceSession.ArticleService;
+
         // GET: Archives
         public ActionResult Index(int id)
         {
-            return View();
+            Article article = _articleService.GetEntity(id);
+
+            //获取博文关键词
+            string[] keywords = null;
+            if(!string.IsNullOrEmpty(article.Keywords))
+            {
+                keywords = article.Keywords.Split(' ');
+            }
+            ViewData["keywords"] = keywords;
+
+            return View(article);
         }
     }
 }
